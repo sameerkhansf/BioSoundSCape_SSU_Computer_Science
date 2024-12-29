c67cd2736a6f4dfc35853cc2a464bb381d92a6bc
harrisb002 <harrisbe@sonoma.edu>
2024-12-29 00:28:15 -0800

    adding ingest data step for ZenMl

--- README.md
+++ README.md
@@ -215,3 +215,10 @@ Distributed under the MIT License. See `LICENSE.txt` for more information.
 [Bootstrap-url]: https://getbootstrap.com
 [JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
 [JQuery-url]: https://jquery.com
+
+
+## Setup
+
+- conda activate zenml_env
+- zenml login --local
+
---
+++ steps/ingest_data.py
@@ -0,0 +1,34 @@
+import logging
+
+import pandas as pd
+from zenml import step
+
+
+class IngestData:
+    """
+    Data ingestion class which ingests data from the source and returns a DataFrame.
+    """
+
+    def __init__(self, data_path: str) -> None:
+        self.data_path = data_path
+
+    def get_data(self) -> pd.DataFrame:
+        logging.info(f"Ingesting data from {self.data_path}")
+        return pd.read_csv(self.data_path)
+
+
+@step
+def ingest_data(data_path: str) -> pd.DataFrame:
+    """
+    Args:
+        data_path: path to the .csv
+    Returns:
+        pd.DataFrame: The ingested data
+    """
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(e)
+        raise e
